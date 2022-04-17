9665ba4cab4b2b23f9411cbb735a41c24f8116f7
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2022-04-17 09:02:31 -0400

    initial commit

--- upload/data_upload.sh
+++ upload/data_upload.sh
@@ -4,6 +4,7 @@
 
 # target syn29442253
 
+ssh dataxfer
 
 DIR=/sc/arion/projects/CommonMind/data/symlinks_for_mondale/RNAseq/*ACC*/Processed/RAPiD/
 
@@ -11,7 +12,7 @@ TMP=/sc/arion/scratch/hoffmg01/tmp
 mkdir $TMP/featureCounts $TMP/kallisto $TMP/rsem $TMP/qc_metrics $TMP/salmon $TMP//star
 
 # syn29442509
-$DIR/bam/*bam
+\ls $DIR/bams/*bam | parallel -P1 synapse add --parentid syn29442509 {}
 
 # syn29443110
 \ls $DIR/featureCounts/* | parallel -P1 cp {} $TMP/featureCounts/
@@ -42,7 +43,7 @@ done
 \ls $TMP/salmon/* | parallel -P1 synapse add --parentid syn29442530 {}
 
 # syn29442534
-\ls $DIR/star/*..Log.final.out | grep -v txrevise | parallel -P1 cp {} $TMP/star/
+\ls $DIR/star/*.Log.final.out | grep -v txrevise | parallel -P1 cp {} $TMP/star/
 \ls $TMP/star/* | parallel gzip
 \ls $TMP/star/* | parallel -P1 synapse add --parentid syn29442534 {}
 
